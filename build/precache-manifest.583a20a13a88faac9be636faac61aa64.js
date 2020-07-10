2bddcb4caa151a366d3a3b8060611ede90aec38a
Katerina <weyulgex@gmail.com>
2020-07-10 21:33:53 +0300

    added corona-happy-hand app

---
+++ build/precache-manifest.583a20a13a88faac9be636faac61aa64.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ca720c998ce50fe833748a654a64af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4626359b874b3a2d1ad6",
+    "url": "/static/css/main.7af0f8c3.chunk.css"
+  },
+  {
+    "revision": "17c446b5e24b4272506e",
+    "url": "/static/js/2.e19b67a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e19b67a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4626359b874b3a2d1ad6",
+    "url": "/static/js/main.ca4e9bb6.chunk.js"
+  },
+  {
+    "revision": "596c9b8282b88c535313",
+    "url": "/static/js/runtime-main.ca566119.js"
+  },
+  {
+    "revision": "cb8a31ef0042add3c17f97bad5086676",
+    "url": "/static/media/addNewList.cb8a31ef.svg"
+  },
+  {
+    "revision": "a2c38e87e1f268c8952f711f510e6eb4",
+    "url": "/static/media/addPhoto.a2c38e87.svg"
+  },
+  {
+    "revision": "d1c71bc6a571b23b3d45c88a5fa560b5",
+    "url": "/static/media/addToBasket.d1c71bc6.svg"
+  },
+  {
+    "revision": "d4273ba0dd19133a39b1eee78a7a2de1",
+    "url": "/static/media/busket.d4273ba0.svg"
+  },
+  {
+    "revision": "093b5e852615e6d0d01768af7de71232",
+    "url": "/static/media/checkMark.093b5e85.svg"
+  },
+  {
+    "revision": "de68fca3040b41b327441cc15695119a",
+    "url": "/static/media/checkedMark.de68fca3.svg"
+  },
+  {
+    "revision": "19d659d9cf75af48e8668ecd4b40a67b",
+    "url": "/static/media/closeCircle.19d659d9.svg"
+  },
+  {
+    "revision": "6545dbb8b38f9a3a5e8a04771f9dd21a",
+    "url": "/static/media/downArrow.6545dbb8.svg"
+  },
+  {
+    "revision": "36f6082ee9b8ec46644362ff179be8ef",
+    "url": "/static/media/eye.36f6082e.svg"
+  },
+  {
+    "revision": "6c86a3f088f6f064ab681860f6cca3d3",
+    "url": "/static/media/favourites.6c86a3f0.svg"
+  },
+  {
+    "revision": "a39d79c29f755575eae4e235728d5d4d",
+    "url": "/static/media/handLike.a39d79c2.svg"
+  },
+  {
+    "revision": "7e514939e189e0e75132c646754f2632",
+    "url": "/static/media/inBasket.7e514939.svg"
+  },
+  {
+    "revision": "832da7aefac81c6b8a17322df7b20614",
+    "url": "/static/media/like.832da7ae.svg"
+  },
+  {
+    "revision": "649069cdaf168e24e0130add5ea65bb0",
+    "url": "/static/media/mainWish.649069cd.png"
+  },
+  {
+    "revision": "4f20b1ba0390c5d8f001b743211d8c4c",
+    "url": "/static/media/nextArrow.4f20b1ba.svg"
+  },
+  {
+    "revision": "3360d136e6df50d413b563d07e17e645",
+    "url": "/static/media/plus.3360d136.svg"
+  },
+  {
+    "revision": "27dc8e91d3a84e471240a3a0fee791f2",
+    "url": "/static/media/prevArrow.27dc8e91.svg"
+  },
+  {
+    "revision": "8bec6a8835ff96e50d35d7f9118b8c61",
+    "url": "/static/media/product.8bec6a88.png"
+  },
+  {
+    "revision": "eb91e76ed8fdfda26ae0dfeff1b9b803",
+    "url": "/static/media/profile.eb91e76e.svg"
+  },
+  {
+    "revision": "4aceb4b1ebc20d0f1c7b221b4b4d41ee",
+    "url": "/static/media/ruslan.4aceb4b1.png"
+  },
+  {
+    "revision": "5479164fb27003eab674de3c0f549e7e",
+    "url": "/static/media/search.5479164f.svg"
+  }
+]);
